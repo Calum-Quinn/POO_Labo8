15203d150a0caec0e5bffe80589ea943ea2ae0e0
calum <calum@quinns.ch>
2023-11-29 13:39:42 +0100

    Add Main Class

---
+++ chess/engine/Main.java
@@ -0,0 +1,34 @@
+package chess.engine;
+
+import chess.ChessController;
+import chess.ChessView;
+import chess.views.gui.GUIView;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Création du contrôleur pour gérer le jeu d'échecs
+        ChessController controller = new ChessController() {
+            @Override
+            public void start(ChessView view) {
+
+            }
+
+            @Override
+            public boolean move(int fromX, int fromY, int toX, int toY) {
+                return false;
+            }
+
+            @Override
+            public void newGame() {
+
+            }
+        };
+
+        // 2. Création de la vue désirée
+        ChessView view = new GUIView(controller);
+        //ChessView view = new ConsoleView(controller); MODE CONSOLE
+
+        // 3. Lancement du programme
+        controller.start(view);
+    }
+}
