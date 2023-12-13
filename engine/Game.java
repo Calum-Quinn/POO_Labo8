7b0f58a025d0e9bd33ee879190864114cfe7cf90
dylanramos <dylan.oliveiraramos@heig-vd.ch>
2023-12-13 14:43:51 +0100

    initialize board

--- engine/Game.java
+++ engine/Game.java
@@ -2,11 +2,13 @@ package engine;
 
 import chess.ChessController;
 import chess.ChessView;
+import chess.PieceType;
 import chess.PlayerColor;
 import engine.piece.*;
 
 public class Game implements ChessController {
     private static final int SIZE = 8;
+    private ChessView view;
     private Piece[][] board;
 
     public Game() {
@@ -15,7 +17,8 @@ public class Game implements ChessController {
 
     @Override
     public void start(ChessView view) {
-
+        this.view = view;
+        this.view.startView();
     }
 
     @Override
@@ -23,9 +26,60 @@ public class Game implements ChessController {
         return false;
     }
 
+    /**
+     * Initialize the board with the pieces at their starting positions.
+     */
     @Override
     public void newGame() {
+        // Pawns
+        for (int i = 0; i < SIZE; i++) {
+            board[i][1] = new Pawn(PlayerColor.WHITE);
+            board[i][6] = new Pawn(PlayerColor.BLACK);
+            view.putPiece(PieceType.PAWN, PlayerColor.WHITE, i, 1);
+            view.putPiece(PieceType.PAWN, PlayerColor.BLACK, i, 6);
+        }
 
+        // Rooks
+        board[0][0] = new Rook(PlayerColor.WHITE);
+        board[7][0] = new Rook(PlayerColor.WHITE);
+        board[0][7] = new Rook(PlayerColor.BLACK);
+        board[7][7] = new Rook(PlayerColor.BLACK);
+        view.putPiece(PieceType.ROOK, PlayerColor.WHITE, 0, 0);
+        view.putPiece(PieceType.ROOK, PlayerColor.WHITE, 7, 0);
+        view.putPiece(PieceType.ROOK, PlayerColor.BLACK, 0, 7);
+        view.putPiece(PieceType.ROOK, PlayerColor.BLACK, 7, 7);
+
+        // Knights
+        board[1][0] = new Knight(PlayerColor.WHITE);
+        board[6][0] = new Knight(PlayerColor.WHITE);
+        board[1][7] = new Knight(PlayerColor.BLACK);
+        board[6][7] = new Knight(PlayerColor.BLACK);
+        view.putPiece(PieceType.KNIGHT, PlayerColor.WHITE, 1, 0);
+        view.putPiece(PieceType.KNIGHT, PlayerColor.WHITE, 6, 0);
+        view.putPiece(PieceType.KNIGHT, PlayerColor.BLACK, 1, 7);
+        view.putPiece(PieceType.KNIGHT, PlayerColor.BLACK, 6, 7);
+
+        // Bishops
+        board[2][0] = new Bishop(PlayerColor.WHITE);
+        board[5][0] = new Bishop(PlayerColor.WHITE);
+        board[2][7] = new Bishop(PlayerColor.BLACK);
+        board[5][7] = new Bishop(PlayerColor.BLACK);
+        view.putPiece(PieceType.BISHOP, PlayerColor.WHITE, 2, 0);
+        view.putPiece(PieceType.BISHOP, PlayerColor.WHITE, 5, 0);
+        view.putPiece(PieceType.BISHOP, PlayerColor.BLACK, 2, 7);
+        view.putPiece(PieceType.BISHOP, PlayerColor.BLACK, 5, 7);
+
+        // Queens
+        board[3][0] = new Queen(PlayerColor.WHITE);
+        board[3][7] = new Queen(PlayerColor.BLACK);
+        view.putPiece(PieceType.QUEEN, PlayerColor.WHITE, 3, 0);
+        view.putPiece(PieceType.QUEEN, PlayerColor.BLACK, 3, 7);
+
+        // Kings
+        board[4][0] = new King(PlayerColor.WHITE);
+        board[4][7] = new King(PlayerColor.BLACK);
+        view.putPiece(PieceType.KING, PlayerColor.WHITE, 4, 0);
+        view.putPiece(PieceType.KING, PlayerColor.BLACK, 4, 7);
     }
 
     public boolean movePiece() {
