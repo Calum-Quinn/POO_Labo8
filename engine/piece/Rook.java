1ad6df315676290cec2572bd33de02bb41669da8
calum <calum@quinns.ch>
2023-12-14 10:59:54 +0100

    Implement Rook validMove without castle

--- engine/piece/Rook.java
+++ engine/piece/Rook.java
@@ -10,6 +10,23 @@ public class Rook extends SpecialPiece {
 
     @Override
     public boolean validMove(int fromX, int fromY, int toX, int toY, Piece[][] board, boolean capture) {
-        return false;
+        // Straight/normal
+        int xDiff = toX - fromX;
+        int yDiff = toY - fromY;
+        if (Math.abs(xDiff) == 0 || Math.abs(yDiff) == 0) {
+            int xSign = xDiff >= 0 ? xDiff == 0 ? 0 : 1 : -1;
+            int ySign = yDiff >= 0 ? yDiff == 0 ? 0 : 1 : -1;
+            // Check no pieces in between
+            for (int i = 1; i < Math.abs(xDiff); ++i) {
+                if (board[fromX + i * xSign][fromY + i * ySign] != null) {
+                    // Castle
+
+
+
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
