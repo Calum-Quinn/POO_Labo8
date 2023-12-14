0eccc5c68ea1f8b6c8a55519eb873e07552b323f
calum <calum@quinns.ch>
2023-12-14 10:20:31 +0100

    Implement bishop validMove

--- engine/piece/Bishop.java
+++ engine/piece/Bishop.java
@@ -10,7 +10,17 @@ public class Bishop extends Piece {
 
     @Override
     public boolean validMove(int fromX, int fromY, int toX, int toY, Piece[][] board, boolean capture) {
-
-        return false;
+        // Check if diagonal move
+        if (Math.abs(fromX - toX) == Math.abs(fromY - toY)) {
+            int xSign = toX - fromX >= 0 ? 1 : -1;
+            int ySign = toY - fromY >= 0 ? 1 : -1;
+            // Check no pieces in between
+            for (int i = 1; i < Math.abs(fromX - toX); ++i) {
+                if (board[fromX + i * xSign][fromY + i * ySign] != null) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
